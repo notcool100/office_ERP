5861a1c5fb32ee2f7a64a1e07d708b6fcba43a08
notcool100 <anjaljoshi6@gmail.com>
2026-06-14 18:27:30 +0545

    fix

--- be/src/api/notification_settings/routes.rs
+++ be/src/api/notification_settings/routes.rs
@@ -5,5 +5,5 @@ use super::handlers::{get_handler, list_handler, update_handler};
 pub fn notification_settings_routes() -> Router {
     Router::new()
         .route("/", get(list_handler))
-        .route("/:key", get(get_handler).put(update_handler))
+        .route("/{key}", get(get_handler).put(update_handler))
 }
